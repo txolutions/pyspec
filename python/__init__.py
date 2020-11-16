fe220aa2cc614c761f37760c20256082a2d35600
Vicente Rey Bakaikoa <txo@txolutions.com>
2020-11-16 15:55:25 -0800

    Add version handling to pyspec

--- python/__init__.py
+++ python/__init__.py
@@ -9,3 +9,8 @@ import os
 _pyspec_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(_pyspec_dir)
 
+from VERSION import getVersion, getFullVersion
+
+__version__ = getVersion()
+__fullversion__ = getFullVersion()
+
